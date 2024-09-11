ea6cdcb9ae07acfd5488d533cfa0ca75593a7a9c
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2024-09-12 00:06:49 +0200

    fixed suspense error from build

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { Suspense } from 'react'
 import { LoginForm } from '@/components/loginForm'
 
 export default function LoginPage() {
@@ -6,7 +7,9 @@ export default function LoginPage() {
     <div className="h-screen w-screen flex justify-center items-center bg-slate-100">
       <div className="sm:shadow-xl px-8 pb-8 pt-12 sm:bg-white rounded-xl space-y-12">
         <h1 className="font-semibold text-2xl">Logga in</h1>
-        <LoginForm />
+        <Suspense fallback={<div>Loading...</div>}>
+          <LoginForm />
+        </Suspense>
         <p className="text-center">
           Har du inget konto?{' '}
           <Link className="text-indigo-500 hover:underline" href="/register">
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,12 +1,15 @@
 import Link from 'next/link'
 import { RegisterForm } from '@/components/registerForm'
+import { Suspense } from 'react'
 
 export default function RegisterPage() {
   return (
     <div className="h-screen w-screen flex justify-center items-center bg-slate-100">
       <div className="sm:shadow-xl px-8 pb-8 pt-12 sm:bg-white rounded-xl space-y-12">
         <h1 className="font-semibold text-2xl">Skapa konto</h1>
-        <RegisterForm />
+        <Suspense fallback={<div>Loading...</div>}>
+          <RegisterForm />
+        </Suspense>
         <p className="text-center">
           Har du redan ett konto?{' '}
           <Link className="text-indigo-500 hover:underline" href="/login">
