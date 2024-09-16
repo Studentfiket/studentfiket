a4a18f44358487f30ce8b5d36827fd0509ec5a0e
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2024-09-17 00:13:15 +0200

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,11 @@
 'use client'
 
-import { Button } from '@/components/ui/button';
-import { generateNewPeriod } from '@/lib/pocketbase';
+// import { Button } from '@/components/ui/button';
+// import { generateNewPeriod } from '@/lib/pocketbase';
 
 function Header() {
-  const startDate = new Date("2024-09-16T08:00:00.000Z");
-  const endDate = new Date("2024-09-20T17:00:00.000Z");
+  // const startDate = new Date("2024-09-16T08:00:00.000Z");
+  // const endDate = new Date("2024-09-20T17:00:00.000Z");
 
   return (
     <header>
