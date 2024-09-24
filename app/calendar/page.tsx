6681d417a54b0d4ce08bb00a865712895755541a
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2024-09-24 07:32:19 +0200

    fix for static page?

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -4,6 +4,15 @@ import CalendarLoading from './calendarLoading';
 import CalendarContainer from './calendarContainer';
 import Footer from '@/components/footer';
 
+// Next.js will invalidate the cache when a
+// request comes in, at most once every second.
+export const revalidate = 1
+
+// We'll prerender only the params from `generateStaticParams` at build time.
+// If a request comes in for a path that hasn't been generated,
+// Next.js will server-render the page on-demand.
+export const dynamicParams = true // or false, to 404 on unknown paths
+
 export default async function Home() {
   return (
     <div className='flex flex-col'>
