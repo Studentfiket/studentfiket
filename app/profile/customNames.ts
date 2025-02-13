2c0286928c6429fe5f8108f16e8f88d858f250b4
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2025-02-13 21:47:37 +0100

    More custom names :)

--- app/profile/customNames.ts
+++ app/profile/customNames.ts
@@ -19,6 +19,19 @@ const customNames: { [key: string]: string | (() => string) } = {
   'moyhj795': "Muuuuu Mjölkfrid",
   'alipa233': "Pelle ”Pelle” Pälverinne",
 
+  // SKURKERIET
+  'johda474': "Captain Jill S. Creu",
+  'alvsu234': "Bree C. Zeyl",
+  'emmah230': "Connie Trast Valoo",
+  'frian635': "Maddy Cash",
+  'joehe737': "Jaques Poirot aka. Webmaister",
+  'leome651': "Sir Von D. Fleet",
+  'linlo121': "Sav Y. May Thea",
+  'annso699': "Wendy Ceglith",
+  'maxsa485': "Lincoln Bayou",
+  'teohe874': "Noa R. Steass",
+  'arvma857': "T. Rolph Ace",
+
   'armab790': "123ArmenAbedi 456GimmeAKiss",
   'svega741': generateSvenGard,
   'ottli370': "Stuvsta!"
